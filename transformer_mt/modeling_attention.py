785382cd6d4131bf24c0854d275e2205b9c872e1
MatriXcel <wasiimouro@gmail.com>
2022-03-21 23:00:31 -0400

    moved model to device

--- transformer_mt/modeling_attention.py
+++ transformer_mt/modeling_attention.py
@@ -97,6 +97,8 @@ class MultiHeadAttention(nn.Module):
             # Our implementation is 3 lines
             # YOUR CODE STARTS HERE
             #scores = scores.reshape((bs * self.num_heads, attending_seq, attended_seq))
+            key_padding_mask = key_padding_mask.to(scores.device)
+
             key_padding_mask = torch.repeat_interleave(key_padding_mask, torch.LongTensor([attending_seq * self.num_heads] * bs), dim=0) \
                 .reshape((bs * self.num_heads, attending_seq, key_padding_mask.shape[1]))
 
